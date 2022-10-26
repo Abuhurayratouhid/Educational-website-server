1abb0ad965d66c5d94340f37ed417c94ce6c573c
Abuhurayratouhid <abuhurayratouhid@gmail.com>
2022-10-25 17:24:09 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+const port = 5000;
+const cors = require('cors')
+
+app.use(cors())
+
+app.get('/',(req, res)=>{
+    res.send('Hello world')
+})
+
+app.listen(port, ()=>{
+    console.log(`educamb server is running on ${port} `)
+})
